71d63e63c4ee7da6cb2eba2d1a32b47fd16c4653
Roberto <lainezroberto02@gmail.com>
2025-02-17 17:36:55 -0600

    ADD Tipos de datos

---
+++ basic/02-tipodedatos.js
@@ -0,0 +1,49 @@
+// Tipos de datos primitivos
+
+// Cadenas de texto (string)
+let myName = "Roberto"
+let alias = "Beto"
+let email = "roberto@gmail.com"
+
+// Numeros (number)S
+let age = 23 //Entero
+let height = 1.80 //Decimal
+
+// Booleanos (boolean)
+let isTeacher = true
+let isStudent = false
+
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+// Null
+let nullValue = null
+
+// Sysbol
+let mySymbol = Symbol("mysymbol") 
+
+// BigInt
+let myBigInt = BigInt (75890123754091724091479018237439457831)
+let myBigInt2 = 75890123754091724091479018237439457831n
+
+// Mostramos los tipos de datos 
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
+
