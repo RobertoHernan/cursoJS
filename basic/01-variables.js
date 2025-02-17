9aab9196734339abfa7c34c85babd37c2e6b781a
Roberto <lainezroberto02@gmail.com>
2025-02-17 17:06:43 -0600

    ADD variables

---
+++ basic/01-variables.js
@@ -0,0 +1,15 @@
+//var 
+
+var helloWorld = "Hola, JavaScript"
+console.log(helloWorld)
+
+//let
+
+let helloWorld2 = "Hola, JavaScript2"
+console.log(helloWorld2)
+
+//const 
+
+/* Las variables creadas con const no pueden ser reasignadas*/
+const helloWorld3 = "Hola, JavaScript3"
+console.log(helloWorld3)
