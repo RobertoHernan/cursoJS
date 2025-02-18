f9d408d71e4b7fe9f0bb044c14f80ae49cbb8d38
Roberto <lainezroberto02@gmail.com>
2025-02-17 18:15:29 -0600

    ADD ejercicios

---
+++ ejercicios/03-primeros-ejercicios.js
@@ -0,0 +1,89 @@
+// 1. Escribe un comentario en una linea
+
+    //Este es un comentario en una sola linea
+
+// 2. Escribe un comentario en varias lineas
+
+    /* Este es el segundo ejercicio
+        de comentar en varias lineas*/
+
+// 3. declara variables con valores asociados a tosos los datos de tipo primitivo
+
+// Tipo string
+    let nombre = "Roberto Lainez"
+
+// Tipo numeric
+    let edad = 23
+
+// Tipo boolean
+    let isSister = true
+    let isUncle = false
+
+// Undefined
+    let undefined 
+
+// Tipo NULL
+    let nullValue = null
+
+// Tipo symbol
+    let mySymbol = Symbol("mysymbol")
+
+// Tipo bigint
+    let BigInt = 18754890175091751075012375012375n
+
+// 4. Imprime por consola el valor de todas las variables
+    // Tipo String
+    console.log(nombre)
+
+    // Tipo Numeric
+    console.log(edad)
+
+    // Tipo Boolean
+    console.log(isSister)
+    console.log(isUncle)
+
+    // Tipo Undefined
+    console.log(undefined)
+
+    // Tipo NULL
+    console.log(nullValue)
+
+    // Tipo symbol 
+    console.log(mySymbol)
+
+    // Tipo BigInt 
+    console.log(BigInt)
+
+// 5. imprimepro consola el tipo de todas las variables
+
+// Tipo String
+    console.log(typeof nombre)
+
+// Tipo Numeric
+    console.log(typeof edad)
+
+// Tipo Boolean
+    console.log(typeof isSister)
+    console.log(typeof isUncle)
+
+// Tipo Undefined
+    console.log(typeof undefined)
+
+// Tipo NULL
+    console.log(typeof nullValue)
+
+// Tipo symbol 
+    console.log(typeof mySymbol)
+
+// Tipo BigInt 
+    console.log(typeof BigInt)
+
+// 6. A continuacion, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuacion, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuacion, modifica los valores de las constantes
+
+// 10. comenta las lineas que produzcan algun tipo de error al ejecutarse 
