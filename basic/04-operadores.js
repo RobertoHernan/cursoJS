2dce2ed51eff981e5241aad7665e76187f0c6ea5
Roberto <lainezroberto02@gmail.com>
2025-02-17 21:19:42 -0600

    ADD operadores

---
+++ basic/04-operadores.js
@@ -0,0 +1,24 @@
+// Operadores
+
+//operadores aritmeticos
+let a = 5
+let b = 10
+
+console.log(a + b) //suma
+console.log(a - b) //resta
+console.log(a / b) //division
+console.log(a * b) //Multiplicacion 
+
+
+console.log(a % b) //modulo
+console.log(a ** b) //exponente
+
+a++ //incremento
+console.log(a)
+
+b-- //decremento
+console.log(b)
+
+//operadores de comparacion
+
+console.log (a > b)
