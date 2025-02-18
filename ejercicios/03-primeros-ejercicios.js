64c0810fcef48509af5a015c161d64dc724338bd
Roberto <lainezroberto02@gmail.com>
2025-02-17 21:10:19 -0600

    ADD terminando ejercicios

--- ejercicios/03-primeros-ejercicios.js
+++ ejercicios/03-primeros-ejercicios.js
@@ -20,7 +20,7 @@
     let isUncle = false
 
 // Undefined
-    let undefined 
+    let myUndefined 
 
 // Tipo NULL
     let nullValue = null
@@ -43,7 +43,7 @@
     console.log(isUncle)
 
     // Tipo Undefined
-    console.log(undefined)
+    console.log(myUndefined)
 
     // Tipo NULL
     console.log(nullValue)
@@ -67,7 +67,7 @@
     console.log(typeof isUncle)
 
 // Tipo Undefined
-    console.log(typeof undefined)
+    console.log(typeof myUndefined)
 
 // Tipo NULL
     console.log(typeof nullValue)
@@ -80,10 +80,94 @@
 
 // 6. A continuacion, modifica los valores de las variables por otros del mismo tipo
 
+    // Tipo String
+        nombre = "Hernan Trejo"
+
+    // Tipo Numeric
+        edad = 24
+
+    // Tipo Boolean
+        isSister = false
+        isUncle = true
+
+    // Tipo Undefined
+        myUndefined = undefined
+
+    // Tipo NULL
+        nullValue = null
+
+    // Tipo symbol 
+        mySymbol = Symbol("newSymbol")
+
+    // Tipo BigInt 
+        BigInt = 3247671892346712893461892468912346n
+
+
 // 7. A continuacion, modifica los valores de las variables por otros de distinto tipo
+
+    nombre = 10
+    console.log(nombre)
+
+    edad = "hola"  
+    console.log(edad)
+
+    isSister = undefined
+    isUncle = 1
+    console.log(isSister)
+    console.log(isUncle)
+
+    // No se puede modificar "undefined"
+    // undefined = "edad"  // ERROR 
+
+    nullValue = true
+    console.log(nullValue)
+
+    mySymbol = 134817359071905710235715n  // Esto no es un symbol, es un BigInt
+    console.log(mySymbol)
+
+    myBigInt = true  // Ahora es un booleano
+    console.log(myBigInt)
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
-// 9. A continuacion, modifica los valores de las constantes
+    const nombre2 = "Juan Luis"
 
+    const age2 = 26
+
+    const isSister2 = true 
+    const isUncle2 = false
+
+    const undefinedValue2 = undefined
+
+    const NullValue2 = null
+
+    const mySymbol2 = Symbol("newSymbol2")
+
+    const BigInt2 = 7125390817509170571095n
+
+// 9. A continuacion, modifica los valores de las constantes
+/*
+    nombre2 = "Carlos Salazar"
+    console.log(nombre2)
+
+    age2 = 14
+    console.log(age2)
+
+    isSister2 = false
+    isUncle2 = true
+    console.log(isSister2)
+    console.log(isUncle2)
+
+    undefinedValue2 = "hoy tengo un valor"
+    console.log(undefinedValue2)
+
+    NullValue2 = 100
+    console.log(NullValue2)
+
+    mySymbol2 = Symbol("otrosymbol")
+    console.log(mySymbol2)
+
+    BigInt2 = 9876543219876543219876n
+    console.log(BigInt2)
+*/
 // 10. comenta las lineas que produzcan algun tipo de error al ejecutarse 
