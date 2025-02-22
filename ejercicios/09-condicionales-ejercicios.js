36f60f05c4712c24b56f16d29f347e98ae82ea88
Roberto <lainezroberto02@gmail.com>
2025-02-21 23:17:27 -0600

    ADD ultimos ejercicios de condicionales

--- ejercicios/09-condicionales-ejercicios.js
+++ ejercicios/09-condicionales-ejercicios.js
@@ -50,7 +50,7 @@ edad >= 18 ? console.log("adulto") : console.log("menor")
 
 // 6. Muestra en que estacion del año nos encontramos dependiendo del valor de una variable "mes"
 
-const mes = 1
+let mes = 1
 
 if (mes == 3 || mes == 4 || mes == 5) {
     console.log("Es primavera")
@@ -63,7 +63,6 @@ if (mes == 3 || mes == 4 || mes == 5) {
 } else {
     console.log("Ingrese un mes valido entre el 1 y 12")
 }
-
 
 // 7. Muestra el numero de dias que tiene un mes dependiendo de la variable del ejercicio anterior 
 
@@ -81,6 +80,69 @@ if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes
 
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma 
 
+let lenguage = "de"
+let mensaje
+
+switch (lenguage) {
+    case "es":
+        mensaje = "Hola"
+        break
+    case "en": 
+        mensaje = "Hello"
+        break
+    case "fr":
+        mensaje = "Bonjour"
+        break
+    case "de":
+        mensaje = "Hallo"
+        break
+    default:
+        mensaje = "Idioma no soportado"
+}
+
+console.log(mensaje)
+
 // 9. Usa un switch para hacer de nuevo el ejercicio 6
 
+mes = 12
+let estacionDeAnio
+
+switch (mes) {
+    case 12: case 1: case 2:
+        estacionDeAnio = "Invierno"
+        break
+    case 3: case 4: case 5:
+        estacionDeAnio = "Primavera"
+        break
+    case 6: case 7: case 8:
+        estacionDeAnio = "Verano"
+        break
+    case 9: case 10: case 11:
+        estacionDeAnio = "Otoño"
+        break
+    default:
+        estacionDeAnio = "El mes no existe ingrese un mes del 1 al 12"
+}
+
+console.log(`El mes ${mes} es: ${estacionDeAnio}`)
+
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+
+mes = 6
+let diasDelMes
+
+switch (mes) {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        diasDelMes = 31
+        break
+    case 4: case 6: case 9: case 11:
+        diasDelMes = 30
+        break
+    case 2:
+        diasDelMes = 28
+        break
+    default:
+        diasDelMes = "El mes no existe ingrese un mes del 1 al 12"
+}
+
+console.log(`El mes ${mes} tiene ${diasDelMes} dias`)
